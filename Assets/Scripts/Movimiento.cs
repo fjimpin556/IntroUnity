8f5ad6ac983d9d41176ffdd876c52659ffc22fe2
fjimpin556 <fjimpin556@g.educaand.es>
2024-11-24 23:53:25 +0100

    Avance domingo

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movimiento : MonoBehaviour
+{
+    int contador = 10;
+    public GameObject itemFinal;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Salir del juego
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Item")
+            {
+                Destroy(other.gameObject);
+                contador -= 1;
+                if (contador == 0)
+                {
+                    itemFinal.SetActive(true);
+                }
+            }
+
+            if (other.gameObject.tag == "Finish")
+            {
+                Destroy(other.gameObject);
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+}
